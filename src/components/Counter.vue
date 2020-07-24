8d7cdbcb4d1c3532e8f79377e7d9e5839bcc1761
Jake Jones <jake.jones@avolanta.com>
2020-07-24 19:49:21 -0400

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        {{count}}
+        <button @click="add">add one</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            add () {
+                this.count++
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
